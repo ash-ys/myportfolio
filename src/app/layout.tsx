b6f9d99d6f66e22912b9ce8c3e00a872c8f94748
Ashish <53848069+ash-ys@users.noreply.github.com>
2026-05-17 21:32:27 +0545

    global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Archivo } from "next/font/google";
-// import "./globals.css";
+ import "./globals.css";
 
 const archivo = Archivo({
   display: "swap",
